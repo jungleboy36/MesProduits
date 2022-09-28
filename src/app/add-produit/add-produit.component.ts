7676aea0a19a0377e62a74ccc9d701c2fe302c38
jungleboy36 <93709766+jungleboy36@users.noreply.github.com>
2022-09-28 19:34:35 +0100

    Initial commit

---
+++ src/app/add-produit/add-produit.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-add-produit',
+  templateUrl: './add-produit.component.html',
+  styleUrls: ['./add-produit.component.css']
+})
+ //un tableau de chînes de caractères
+export class AddProduitComponent implements OnInit {
+
+  constructor() { 
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+}
+
